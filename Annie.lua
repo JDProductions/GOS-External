08ee44f10b23677e8482f2477770a1f4482c56b8
JamesDuBois1994 <jamesdubois@oeconnection.com>
2017-02-23 01:32:43 -0500

    Update - 1.01
    Removed old Orbwalker, added near perfect Q farm and more etc etc

--- Annie.lua
+++ Annie.lua
@@ -1,21 +1,29 @@
 require("DamageLib")
+
 if myHero.charName ~= "Annie" then
   return
 end
 
-local path = SCRIPT_PATH.."ExtLib.lua"
+local path = SCRIPT_PATH.."eXternal Orbwalker.lua"
 
 if FileExist(path) then
   _G.Enable_Ext_Lib = true
   loadfile(path)()
 else
-  print("ExtLib Not Found. You need to install ExtLib before using this script")
+  print("eXternal Orbwalker Not Found. You need to install eXternal Orbwalker before using this script")
   return
 end 
 
-local Q = {Delay = 0, Radius = 0, Range = 625, Speed = math.huge}
-local W = {Delay = 0, Radius = 0, Range = 625, Speed = math.huge}
-local R = {Delay = 0, Radius = 10, Range = 600, Speed = math.huge}
+PrintChat("[DOOBIES ANNIE] Welcome. and please report bugs on the forums.")
+
+ Q = {Delay = 0, Radius = 0, Range = 625, Speed = math.huge}
+ W = {Delay = 0, Radius = 0, Range = 625, Speed = math.huge}
+ R = {Delay = 0, Radius = 10, Range = 600, Speed = math.huge}
+
+local PASSIVE_AMMO_MAX = myHero.hudMaxAmmo
+local currentAmmo = myHero.hudAmmo
+IsPassiveReady = false
+
 local Ts = TargetSelector
 local _prediction = Prediction
 
@@ -26,6 +34,7 @@ AnnieMenu:MenuElement({type = MENU, id = "Combo", name = "Combo Settings"})
 AnnieMenu.Combo:MenuElement({id = "UseQ", name = "Use Q", value = true})
 AnnieMenu.Combo:MenuElement({id = "UseW", name = "Use W", value = true})
 AnnieMenu.Combo:MenuElement({id = "UseR", name = "Use R", value = true})
+
 
 -- Last Hit orbwalker menu
 AnnieMenu:MenuElement({type = MENU, id = "LastHit", name = "Last Hit Settings"})
@@ -64,64 +73,65 @@ function GetBuffs(unit)
     return T
 end
 
+function CheckForPassiveBuff()
+        if currentAmmo < PASSIVE_AMMO_MAX then
+            IsPassiveReady = false
+        elseif currentAmmo == PASSIVE_AMMO_MAX then
+            PrintChat("Annie Passive Is Ready")
+        end
 
-OnActiveMode(function (OW, Minions)
-  if OW.Mode == "Combo" then
-    Combo(OW)
-  elseif OW.Mode == "LastHit" then  
-    LastHit(OW,Minions)
-  end
-end)
- 
+        -- if passive is true then set the value of last hit q farm to false
+end
+
+
 function IsReady(slot)
   if  myHero:GetSpellData(slot).currentCd == 0 and myHero.mana > myHero:GetSpellData(slot).mana and myHero:GetSpellData(slot).level > 0 then
     return true
   end
   return false
 end
- 
- 
-function DisableOrb(OW)
-  OW.enableAttack = false
-  OW.enableMove = false
+
+function GetEnemyMinions()
+	EnemyMinions = {}
+	for i = 1, Game.MinionCount() do
+		local Minion = Game.Minion(i)
+		if Minion.isEnemy then
+			table.insert(EnemyMinions, Minion)
+		end
+	end
+	return EnemyMinions
 end
 
-function EnableOrb(OW)
-  OW.enableAttack = true
-  OW.enableMove = true
-end
-
-function Combo(OW)
-  local qTarget = Ts:GetTarget(Q.Range)
-  local wTargets = Ts:GetTarget(W.Range)
-  local rTargets = Ts:GetTarget(R.Range)
-  
-  if qTarget and Game.CanUseSpell(_Q) == READY and AnnieMenu.Combo.UseQ:Value() then
-    Control.CastSpell(HK_Q,qTarget)
+function Combo()
+local target = EOW:GetTarget()
+  if EOW:Mode() == "Combo" then
+  if target and Game.CanUseSpell(_Q)and IsReady(_Q) and AnnieMenu.Combo.UseQ:Value() then
+    Control.CastSpell(HK_Q,target)
     end
     
-    if wTargets and Game.CanUseSpell(_W) == READY and AnnieMenu.Combo.UseW:Value()then
-      local CastPosition, Hitchance = _prediction:GetPrediction(wTargets,W)
-        if Hitchance == "High" then
-        SpellCast:CastSpell(HK_W, CastPosition)--extlib support cast skillshot
-          end
+    if target and Game.CanUseSpell(_W) and IsReady(_W) and AnnieMenu.Combo.UseW:Value()then
+			local Prediction = target:GetPrediction(Q.Speed, Q.Delay)
+			Control.CastSpell(HK_W, Prediction)
      end
      
-    if rTargets and Game.CanUseSpell(_R) and IsReady(_R) and AnnieMenu.Combo.UseR:Value() then
-    local CastPosition, Hitchance = _prediction:GetPrediction(rTargets, R)
-      if Hitchance == "High" then
-        SpellCast:CastSpell(HK_R, CastPosition)
-        end
+    if target and Game.CanUseSpell(_R) and IsReady(_R) and AnnieMenu.Combo.UseR:Value()then
+			local Prediction = target:GetPrediction(R.Speed, R.Delay)
+			Control.CastSpell(HK_R, Prediction)
      end
+ end
  end -- end function Combo
+Callback.Add("Tick", Combo);
 
  
-function LastHit(OW, Minions)
-  local qTarget = Ts:GetTarget(Q.range)
+function LastHit()
+  --CheckForPassiveBuff()
+  Minions = {GetEnemyMinions()}
+  if EOW.Mode() == "LastHit" then
   for i, minion in pairs (Minions[1]) do--should check passive buff before lasthit
-    if minion and IsReady(_Q) and IsValidTarget(minion, 625, false, myHero.pos, minion) and getdmg("Q",minion,myHero) > minion.health  then
+    if minion and AnnieMenu.LastHit.UseQ:Value()  and  IsReady(_Q) and IsValidTarget(minion, 625, false, myHero.pos, minion) and getdmg("Q",minion,myHero) > minion.health  then
       Control.CastSpell(HK_Q, minion)--Q is targeted spell, no need prediction here
-
     end
   end
 end
+end
+Callback.Add("Tick", LastHit);
